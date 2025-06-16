67ac1ab943f685d2b2d6f5d73f9f82b1176bb4f5
Luiz Fernando <lfmenezes22@gmail.com>
2025-06-16 14:56:10 -0300

    Create repo

---
+++ src/main/java/com/prestacaoservicos/ApiServicosP1Application.java
@@ -0,0 +1,13 @@
+package com.prestacaoservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServicosP1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiServicosP1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/prestacaoservicos/ApiServicosP1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prestacaoservicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServicosP1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
