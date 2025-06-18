eb9c5a5812b98b38d9bd35aec4e37af03da1f45f
Douglas Cunha <douglas.jesus077@academico.ifs.edu.br>
2025-06-17 22:35:06 -0300

    feat: add initial files and configs

---
+++ src/main/java/com/prestacaoservicos/config/OpenAPIConfig.java
@@ -0,0 +1,15 @@
+package com.prestacaoservicos.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("API de Agendamento de Serviços").version("1.0").description("Documentação Swagger"));
+    }
+}
