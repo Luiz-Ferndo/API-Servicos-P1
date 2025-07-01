39ac2eaa04a1626a86e0461dd212d62527218cd3
Luiz Fernando <lfmenezes22@gmail.com>
2025-07-01 11:12:42 -0300

    feat: add controller servicos

---
+++ src/main/java/com/prestacaoservicos/controller/ServicosController.java
@@ -0,0 +1,10 @@
+package com.prestacaoservicos.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/servicos")
+public class ServicosController {
+}
