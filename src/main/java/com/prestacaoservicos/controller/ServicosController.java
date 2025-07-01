b558083ef44549d2041175eb3182e7b5b129b149
Luiz Fernando <lfmenezes22@gmail.com>
2025-07-01 12:12:56 -0300

    Feat: add servicos Controller

--- src/main/java/com/prestacaoservicos/controller/ServicosController.java
+++ src/main/java/com/prestacaoservicos/controller/ServicosController.java
@@ -1,10 +1,41 @@
 package com.prestacaoservicos.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.prestacaoservicos.entity.Servicos;
+import com.prestacaoservicos.service.ServicosService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/servicos")
 public class ServicosController {
+
+    private ServicosService servicosService;
+
+
+    public List<Servicos> listaServicos(){
+        return servicosService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Servicos> listaServicosPorId(Long id){
+        return servicosService.buscarPorId(id);
+    }
+
+    @PostMapping
+    public Servicos criarServico(Servicos servicos) {
+        return servicosService.salvar(servicos);
+    }
+
+    @PutMapping("/{id}")
+    public Servicos atualizarServico(@PathVariable Long id, @RequestBody Servicos servicos) {
+        return servicosService.atualizar(id, servicos);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarServico(@PathVariable Long id) {
+        servicosService.deletar(id);
+    }
 }
