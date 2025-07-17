ccc3b141d40169a0ecd944ffcffb470a82ce750d
Luiz <lfmenezes22@gmail.com>
2025-07-16 22:32:13 -0300

    feat: create client service

--- src/main/java/com/prestacaoservicos/repository/ServicosRepository.java
+++
@@ -1,7 +0,0 @@
-package com.prestacaoservicos.repository;
-
-import com.prestacaoservicos.entity.Servicos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServicosRepository extends JpaRepository<Servicos, Long> {
-}
