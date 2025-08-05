fb81d20a61f200e34bb1a096cddfff0d6796e51f
Douglas <douglas.jesus077@academico.ifs.edu.br>
2025-08-05 20:57:21 -0300

    feat: enhance user role management and error handling; add new exception handler and update user DTO to use role names

--- src/main/java/com/prestacaoservicos/enums/RoleNameEnum.java
+++ src/main/java/com/prestacaoservicos/enums/RoleNameEnum.java
@@ -2,7 +2,8 @@ package com.prestacaoservicos.enums;
 
 public enum RoleNameEnum {
     ROLE_CUSTOMER,
-    ROLE_ADMINISTRATOR;
+    ROLE_ADMINISTRATOR,
+    ROLE_SERVICE_PROVIDER;
 
     public String toUpperCase() {
         return this.name().toUpperCase();
